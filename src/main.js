6ddd05e37b36b159c9bd714de7fa53f2ea0ccdbc
zichen-jushi <1393885301@qq.com>
2020-12-25 16:20:20 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/*
+入口js
+ */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
